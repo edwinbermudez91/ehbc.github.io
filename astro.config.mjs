4238afcce3e685fc144219f554da057e2a47761c
Edwin Bermudez <99342909+edwinbermudez91@users.noreply.github.com>
2025-06-08 21:20:12 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ehbc.github.io",
+  site: "https://edwinbermudez91.github.io",
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
